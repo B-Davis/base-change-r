f65af8ec27318762c53dc53128136e3e29be59d9
Brian Davis <broadwaybrian@gmail.com>
2015-10-13 15:45:28 -0700

    Update BaseChange.r

--- BaseChange.r
+++ BaseChange.r
@@ -3,13 +3,9 @@ base.change <- function(x,base.origin,base.out) {
   x <- strsplit(x,"")
   x <- x[[1]]
   b <- cbind(rep(10:35,2),c(letters,LETTERS))
-  x[x %in% b[,2]] <-
-    unlist(lapply(1:length(x),function(i)
-      b[x[i] == b[,2],1]))
+  x[x %in% b[,2]] <-unlist(lapply(1:length(x),function(i) b[x[i] == b[,2],1]))
   x <- as.numeric(x)
-  x <- sum((base.origin ^ ((length(
-    x
-  ) - 1):0)) * x)
+  x <- sum((base.origin ^ ((length(x) - 1):0)) * x)
   while (x[length(x)] %/% base.out != 0) {
     x[length(x) + 1] <- x[length(x)] %/% base.out
   }
